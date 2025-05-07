f18eae4eb2029d9b99fd3938c3b436635c3a5161
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-05-07 16:38:06 +0900

    컴포넌트 어노테이션 제거

--- src/main/java/com/gram/eureka/eureka_gram_user/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/gram/eureka/eureka_gram_user/filter/JwtAuthenticationFilter.java
@@ -10,13 +10,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
 
-@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
